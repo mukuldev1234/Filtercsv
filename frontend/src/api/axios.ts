5e7f03bca361e4da6812e6a9caddb40219eab564
mukuldev1234 <57930918+mukuldev1234@users.noreply.github.com>
2026-05-17 22:38:59 +0530

    Update axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-leads-backend-u1bi.onrender.com/",
 });
 
 // 🔥 REQUEST INTERCEPTOR
