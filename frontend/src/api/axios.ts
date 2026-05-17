7741b21a0a3a47d95a7ec431ae184deb37100cc7
mukuldev1234 <57930918+mukuldev1234@users.noreply.github.com>
2026-05-17 23:02:11 +0530

    Update axios.ts

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smart-leads-backend-u1bi.onrender.com/",
+  baseURL: "https://smart-leads-backend-u1bi.onrender.com/api",
 });
 
 // 🔥 REQUEST INTERCEPTOR
